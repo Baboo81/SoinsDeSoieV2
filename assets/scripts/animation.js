5c20b57551286ecaa145127e77aac66331bc66f6
Baboo81 <magicbaboo@gmail.com>
2023-11-17 14:59:09 +0100

    css

--- assets/scripts/animation.js
+++ assets/scripts/animation.js
@@ -11,7 +11,7 @@ ball.style.position = "absolute";
 
 function ballAnimate1 () {
 
-    if (leftPos == 900) {
+    if (leftPos == 1200) {
         dir = 1
     } else if 
         (leftPos == -100) {
@@ -28,34 +28,9 @@ function ballAnimate1 () {
     requestAnimationFrame(ballAnimate1);
 };
 
-function ballAnimate2 () {
-
-      if (upDownPos == 900) {
-        dir = 1
-      } else if 
-         (upDownPos == -100) {
-        dir = -1
-      }
-
-      upDownPos += -2 * dir;
-      current_rotation += 1;
-
-
-      ball.style.top = `${upDownPos}px`;
-      ball.style.transform = 'rotate(' + current_rotation + 'deg)';
-
-      requestAnimationFrame(ballAnimate2);
-   
-};
-
-function getRandom () {
-
-      return Math.floor(Math.random(ballAnimate1, ballAnimate2));
-};
-
 requestAnimationFrame(ballAnimate1);
-requestAnimationFrame(ballAnimate2);
-getRandom();
+
+
 
 
 
