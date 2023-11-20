a208203a3d455984101a7e42f9296da87e75876f
Baboo81 <magicbaboo@gmail.com>
2023-11-20 20:01:13 +0100

    Script js carousel-btn X

--- assets/scripts/carouselProduct.js
+++ assets/scripts/carouselProduct.js
@@ -32,3 +32,36 @@ document.querySelector(".carousel-control-next").addEventListener("click", funct
   }
   showSlide(currentSlide);
 });
+
+//fct afficher txt (btn carouselProduct):
+const carouselBtn1 = document.getElementById('carousel-btn1');
+const baliseP1 = document.getElementById('p1');
+const carouselBtn2 = document.getElementById('carousel-btn2');
+const baliseP2 = document.getElementById('p2');
+const carouselBtn3 = document.getElementById('carousel-btn3');
+const baliseP3 = document.getElementById('p3');
+
+
+carouselBtn1.addEventListener("click", () => {
+  if(getComputedStyle(baliseP1).display != "none") {
+      baliseP1.style.display = "none";
+  } else {
+      baliseP1.style.display = "block";
+  }
+});
+
+carouselBtn2.addEventListener("click", () => {
+  if(getComputedStyle(baliseP2).display != "none") {
+      baliseP2.style.display = "none";
+  } else {
+      baliseP2.style.display = "block";
+  }
+});
+
+carouselBtn3.addEventListener("click", () => {
+  if(getComputedStyle(baliseP3).display != "none") {
+     baliseP3.style.display = "none";
+  } else {
+    baliseP3.style.display = "block";
+  }
+});
