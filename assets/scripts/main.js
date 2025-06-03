6093551e03d0c1529926eb484078643498bc9769
Baboo81 <magicbaboo@gmail.com>
2025-06-03 15:53:21 +0200

    Test parallaxe

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,34 +1,34 @@
 "use strict";
 
 //Animation balls:
-const ball = document.getElementById('logo2');
-let leftPos = 0;
-let upDownPos = 0;
-let dir = -1;
-let current_rotation = 0; 
+//const ball = document.getElementById('logo2');
+//let leftPos = 0;
+//let upDownPos = 0;
+//let dir = -1;
+//let current_rotation = 0; 
 
-ball.style.position = "absolute";
+//ball.style.position = "absolute";
 
-function ballAnimate1 () {
+//function ballAnimate1 () {
 
-    if (leftPos == 1200) {
-        dir = 1
-    } else if 
-        (leftPos == -100) {
-        dir = -1
-    }
+    //if (leftPos == 1200) {
+      //  dir = 1
+    //} else if 
+      //  (leftPos == -100) {
+       // dir = -1
+    //}
     
-    leftPos += -2 * dir;
-    current_rotation += 1;
+    //leftPos += -2 * dir;
+    //current_rotation += 1;
     
 
-    ball.style.left = `${leftPos}px`;
-    ball.style.transform = 'rotate('  + current_rotation + 'deg)';
+    //ball.style.left = `${leftPos}px`;
+   // ball.style.transform = 'rotate('  + current_rotation + 'deg)';
 
-    requestAnimationFrame(ballAnimate1);
-};
+    //requestAnimationFrame(ballAnimate1);
+//};
 
-requestAnimationFrame(ballAnimate1);
+//requestAnimationFrame(ballAnimate1);
 //////////////////////////////////////
 //Cards:
 //Card content collapse:
